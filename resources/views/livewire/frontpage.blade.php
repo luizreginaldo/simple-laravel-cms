68d965575a3f33287eeab8eed6b84a0f85a55293
Luiz Reginaldo Silva <contato@luizreginaldo.com>
2021-09-28 20:58:52 -0300

    The first adjustments on the front page using TailwindCSS

--- resources/views/livewire/frontpage.blade.php
+++ resources/views/livewire/frontpage.blade.php
@@ -1,4 +1,13 @@
 <div>
-    <h1>{{ $title }}</h1>
-    {!! $content !!}
+    <div class="border p-5 text-gray-100 bg-gray-500 text-3xl sm:bg-blue-500 md:bg-red-500 lg:bg-yellow-400 xl:bg-green-500">
+        {{ $title }}
+    </div>
+    <div class="lg:flex">
+        <div class="border p-5 text-center sm:text-left lg:w-1/2">
+            {!! $content !!}
+        </div>
+        <div class="border bg-gray-400 lg:w-1/2">
+            <img class="w-full h-full object-cover object-center" src="{{'img/cake.jpg'}}" alt="">
+        </div>
+    </div>
 </div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,9 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
-    ]);
+    ])
+    .browserSync('http://localhost:8000')
+;
 
 if (mix.inProduction()) {
     mix.version();
